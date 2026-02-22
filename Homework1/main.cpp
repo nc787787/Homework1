d2710628f4dd898de85a6ce6aa405c2b2775b03f
chris <noonanc787@macomb.edu>
2026-02-21 21:35:29 -0500

    week 6 changes

--- Homework1/main.cpp
+++ Homework1/main.cpp
@@ -1,10 +1,79 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
 
 using namespace std;
 
 enum ViewingType { THEATER = 1, STREAMING, PHYSICAL };
+
+template <typename T>
+void swapItems(T& a, T& b) {
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+template <typename T>
+class DynamicArray {
+private:
+    T* data;
+    int size;
+    int capacity;
+
+    void resize() {
+        capacity *= 2;
+        T* temp = new T[capacity];
+        for (int i = 0; i < size; i++)
+            temp[i] = data[i];
+        delete[] data;
+        data = temp;
+    }
+
+public:
+    DynamicArray() : size(0), capacity(5) {
+        data = new T[capacity];
+    }
+
+    ~DynamicArray() {
+        delete[] data;  // If T is a pointer, actual object deletion happens in MediaTracker
+    }
+
+    bool add(const T& item) {
+        if (size == capacity)
+            resize();
+        else if (size > capacity) return false;
+        data[size++] = item;
+        return true;
+    }
+
+    bool remove(int index) {
+        if (index < 0 || index >= size)
+            return false;
+        for (int i = index; i < size - 1; i++)
+            data[i] = data[i + 1];
+        size--;
+        return true;
+    }
+
+    T& operator[](int index) {
+        if (index < 0 || index >= size) {
+            cout << "Invalid index\n";
+            static T dummy;  // safe fallback
+            return dummy;
+        }
+        return data[index];
+    }
+
+    const T& operator[](int index) const {
+        return data[index];
+    }
+
+    int getSize() const { return size; }
+};
+
 
 //used as composition
 class Rating {
@@ -53,6 +122,17 @@ public:
         viewingType = v;
     }
 
+    virtual void toStream(ostream& os) const {
+        os << left << setw(20) << title
+            << setw(8) << year
+            << setw(12) << getType();
+    }
+
+    friend ostream& operator<<(ostream& os, const MediaItem& m) {
+        m.toStream(os);
+        return os;
+    }
+
     void setTitle(string t) { title = t; }
     void setYear(int y) { if (y > 0) year = y; }
     void setViewingType(ViewingType v) { viewingType = v; }
@@ -83,6 +163,20 @@ public:
     Film(string t, int y, ViewingType v, double r)
         : MediaItem(t, y, v) {
         rating = r;
+    }
+
+    bool operator==(const Film& other) const {
+        return title == other.title && getYear() == other.getYear() && getRating() == other.getRating();
+    }
+
+    void toStream(ostream& os) const override {
+        MediaItem::toStream(os);
+        os << setw(15) << fixed << setprecision(1) << rating.get();
+        if (rating.isFavorite()) os << "(Fav)";
+    }
+
+    Film* clone() const {
+        return new Film(*this);
     }
 
     void setRating(double r) { rating.set(r); }
@@ -117,6 +211,11 @@ public:
         subject = s;
     }
 
+    void toStream(ostream& os) const override {
+        MediaItem::toStream(os);
+        os << setw(15) << subject;
+    }
+
     void setSubject(string s) { subject = s; }
     string getSubject() const { return subject; }
 
@@ -136,20 +235,7 @@ public:
 //manager class
 class MediaTracker {
 private:
-    MediaItem** items;
-    int size;
-    int capacity;
-
-    void resizeArray() {
-        capacity *= 2;
-        MediaItem** temp = new MediaItem * [capacity];
-
-        for (int i = 0; i < size; i++)
-            temp[i] = items[i];
-
-        delete[] items;
-        items = temp;
-    }
+    DynamicArray<MediaItem*> items;
 
     ViewingType askViewingType() {
         int choice;
@@ -164,48 +250,44 @@ private:
 
 public:
     MediaTracker() {
-        capacity = 5;
-        size = 0;
-        items = new MediaItem * [capacity];
     }
 
     ~MediaTracker() {
-        for (int i = 0; i < size; i++)
-            delete items[i];      
-
-        delete[] items;  
+        for (int i = 0; i < items.getSize(); i++)
+            delete items[i];
     }
 
-    bool add(MediaItem* m) {
-        if (size == capacity)
-            resizeArray();
-        else if (size > capacity) return false;
-        items[size++] = m;
-        return true;
+    MediaTracker& operator+=(MediaItem* m) {
+        items.add(m);
+        return *this;
     }
 
-    bool remove(int index) {
-        if (index < 0 || index >= size)
-            return false;
+    MediaTracker& operator-=(int index) {
+        if (index < 0 || index >= items.getSize()) return *this;
 
         delete items[index];
+        items.remove(index);
+        return *this;
+    }
 
-        for (int i = index; i < size - 1; i++)
-            items[i] = items[i + 1];
-
-        size--;
-        return true;
+    MediaItem* get(int index) const {
+        if (index < 0 || index >= items.getSize()) return nullptr;
+        return items[index];
     }
 
     void printAll() const {
         cout << left << setw(20) << "Title"
-            << setw(8) << "Year"
-            << setw(12) << "Type"
-            << setw(15) << "Rating/Subject" << endl;
+             << setw(8) << "Year"
+             << setw(12) << "Type"
+             << setw(15) << "Rating/Subject" << endl;
         cout << string(55, '-') << endl;
 
-        for (int i = 0; i < size; i++)
-            items[i]->print();   // polymorphism
+        for (int i = 0; i < items.getSize(); i++) {
+            cout << *(items[i]);
+            cout << endl;
+        }
+            
+
         cout << endl;
     }
 
@@ -223,7 +305,7 @@ public:
                 cout << "Year: "; cin >> y;
                 cout << "Rating: "; cin >> r;
                 ViewingType v = askViewingType();
-                add(new Film(t, y, v, r));
+                *this += new Film(t, y, v, r);
             }
 
             if (choice == 2) {
@@ -233,9 +315,9 @@ public:
                 cin.ignore();
                 cout << "Subject: "; getline(cin, s);
                 ViewingType v = askViewingType();
-                Documentary d(t, y, v, s);
-                add(new Documentary(t, y, v, s));
+                *this += new Documentary(t, y, v, s);
             }
+
             if (choice == 3) printAll();
 
             if (choice == 4) {
@@ -243,12 +325,12 @@ public:
                 printAll();
                 cout << "Enter index to remove: ";
                 cin >> index;
-                remove(index);
+                *this -= index;
             }
         } while (choice != 9);
     }
 };
-
+/*
 #ifdef _DEBUG
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
@@ -286,19 +368,80 @@ TEST_CASE("Polymorphic virtual function test") {
     delete m;
 }
 
-TEST_CASE("Add & remove dynamic") {
+TEST_CASE("Add & remove dynamic via += / -=") {
     MediaTracker t;
-    t.add(new Film("A", 2000, STREAMING, 5));
-    t.add(new Documentary("B", 2001, THEATER, "Nature"));
-    CHECK(t.remove(0) == true);
+    t += new Film("A", 2000, STREAMING, 5);
+    t += new Documentary("B", 2001, THEATER, "Nature");
+
+    CHECK(t.get(0) != nullptr);
+    CHECK(t.get(1) != nullptr);
+    t -= 0;
+    CHECK(t.get(0) != nullptr);
+    CHECK(t.get(1) == nullptr);
 }
 
-#else
+TEST_CASE("Film == overload test") {
+    Film f1("Movie", 2020, STREAMING, 8);
+    Film f2("Movie", 2020, STREAMING, 8);
+    Film f3("Other", 2020, STREAMING, 5);
+
+    CHECK(f1 == f2);
+    CHECK(!(f1 == f3));
+}
+
+TEST_CASE("operator<<") {
+    Film f("Movie", 2021, STREAMING, 9);
+    Documentary d("Earth", 2020, THEATER, "Nature");
+    ostringstream oss1, oss2;
+    oss1 << f;
+    oss2 << d;
+    CHECK(oss1.str().find("Movie") != string::npos);
+    CHECK(oss2.str().find("Earth") != string::npos);
+}
+
+//using int because its a class template
+TEST_CASE("DynamicArray operator[]") {
+    DynamicArray<int> arr;
+    arr.add(10);
+    arr.add(20);
+    CHECK(arr[0] == 10);
+    CHECK(arr[1] == 20);
+
+    CHECK(arr[5] == 0);
+}
+
+TEST_CASE("swapItems with Film objects") {
+    Film f1("Movie A", 2000, STREAMING, 7.5);
+    Film f2("Movie B", 2010, THEATER, 8.2);
+
+    swapItems(f1, f2);
+    CHECK(f1.getTitle() == "Movie B");
+    CHECK(f2.getTitle() == "Movie A");
+}
+
+TEST_CASE("DynamicArray auto-resize") {
+    DynamicArray<int> arr;
+    for (int i = 0; i < 10; i++)
+        arr.add(i);
+
+    CHECK(arr.getSize() == 10);
+    for (int i = 0; i < 10; i++)
+        CHECK(arr[i] == i);
+}
+
+
+#else*/
 //main
 int main() {
-    MediaTracker tracker;
-    tracker.showMenu();
+    _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
+    {
+        MediaTracker tracker;
+        tracker.showMenu();
+    }
+    //int* test = new int;
+    //*test = 10;
+    _CrtDumpMemoryLeaks();
     return 0;
 }
 
-#endif
+//#endif
